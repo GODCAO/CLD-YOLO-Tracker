351ab738e90f4fb283bf0cfa0d24c2a904c76119
GODCAO <995160683@qq.com>
2026-07-13 13:17:58 +0800

    Update common_config.py

--- MOT/common_config.py
+++ MOT/common_config.py
@@ -1,39 +1,10 @@
-<<<<<<< HEAD
 # 路径
 import os
 
 # ===== 基本路径 =====
 dataset_root = ''
-splits = ['train','val', 'test']
-
-img_dir_name = 'img1'
-img_ext = '.jpg'
-frame_rate = 30
-
-# ===== YOLO =====
-# model path
-model_path = (
-    ''
-)
-# tracker_config = 'botsort.yaml'
-# # bytetrack  botsort
-
-# ===== TrackEval =====
-trackeval_root = 'TrackEval'
-trackeval_gt_root = os.path.join(
-    trackeval_root, 'data/gt/mot_challenge/MOT_dataset'
-)
-trackeval_tracker_root = os.path.join(
-    trackeval_root, 'data/trackers/mot_challenge/MOT_dataset'
-)
-=======
-# 路径
-import os
-
-# ===== 基本路径 =====
-dataset_root = ''
-# splits = ['train','val', 'test']
-splits = ['val', 'test']
+# splits = ['train','val']
+splits = ['val']
 img_dir_name = 'img1'
 img_ext = '.jpg'
 frame_rate = 30
